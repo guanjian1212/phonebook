ec87b5a8df6ade3191e64606cdfc5afe3cd32816
guanjian <guanjian_1212@163.com>
2023-12-15 16:13:30 +0800

    3.3 done

--- index.js
+++ index.js
@@ -24,9 +24,21 @@ let persons = [
   }
 ]
 
+app.use(express.json());
+
 app.get('/api/persons', (request, response) => {
   response.json(persons)
 })
+
+app.get('/api/persons/:id', (request, response) => {
+  const person = persons.find(person => person.id === Number(request.params.id));
+  if (person) {
+    response.json(person);
+  } else {
+    response.status(404).end();
+  }
+})
+
 
 app.get('/info', (request, response) => {
   response.send(`
